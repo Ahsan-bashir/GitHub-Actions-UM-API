05d7344d8ba28aa97d3ddc868be5897703db47ef
Ahsan-bashir <dev.ahsen.ali@gmail.com>
2025-11-18 11:58:07 +0500

    removed bad code file

--- src/badCode.js
+++
@@ -1,9 +0,0 @@
-// This file has intentional linting errors
-// const unusedVariable = "test"  // Missing semicolon, unused variable
-
-// function badFunction( ){  // Bad spacing
-// var oldStyle = 'use const'  // Use const instead of var
-//   if(true){console.log("bad formatting")}  // Bad formatting
-// }
-
-// module.exports=badFunction  // Missing semicolon
