5f638e0d6e74bccbf2cf1e413238cdcff942cd13
Ahsan-bashir <dev.ahsen.ali@gmail.com>
2025-11-18 11:33:05 +0500

    fixed the issues in badCode.js

--- src/badCode.js
+++ src/badCode.js
@@ -1,9 +1,12 @@
 // This file has intentional linting errors
-const unusedVariable = "test"  // Missing semicolon, unused variable
+// Missing semicolon, unused variable
 
-function badFunction( ){  // Bad spacing
-var oldStyle = 'use const'  // Use const instead of var
-  if(true){console.log("bad formatting")}  // Bad formatting
+function badFunction() {
+  // Bad spacing
+  // Use const instead of var
+  //   if (true) {
+  //     console.log('bad formatting');
+  //   } // Bad formatting
 }
 
-module.exports=badFunction  // Missing semicolon
+module.exports = badFunction; // Missing semicolon
